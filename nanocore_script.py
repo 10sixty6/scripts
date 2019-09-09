8bdd0a0ed28cd742f1fd59f69e58aee15a62a814
tbxtbxtbx <tbx1066@gmail.com>
2019-09-09 16:16:20 +0100

    added guid folder structure checking etc

--- nanocore_script.py
+++ nanocore_script.py
@@ -55,20 +55,45 @@ with open(stringsfilepath, "r") as input:
         	outputfile.write(next(input))
         	outputfile.write(next(input))
         	outputfile.write(next(input))
-        	input.close()
         	print('\nPotential C2 found and written to ' + filepath)
         	break
+
+#get machine guid
+guidregfull = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography\\MachineGuid'
+guidreg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+guidkey = winreg.OpenKey(guidreg, r"SOFTWARE\\Microsoft\\Cryptography\\", access=winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
+rawmachguid = winreg.QueryValueEx(guidkey, "MachineGuid")
+firstguid = str(rawmachguid[0])
+winreg.CloseKey(guidreg)
+
+# check for existence of appdata guid folder & save to file
+appdatapath = os.environ['APPDATA']
+guidpath = os.path.join(appdatapath, firstguid)
+print('\n\nChecking for created files in : ' + guidpath)
+
+# recursive folder contents thanks to https://stackoverflow.com/users/1226857/dhobbs
+if os.path.exists(guidpath) == False:
+	print('MachineGuid Folder Path doesnt exist.. \n')
+else:
+	outputfile.write('\n\nFound folder structure: ' + guidpath + '\n\n' + 'Contents below:\n')
+	for root, dirs, files in os.walk(guidpath):
+		level = root.replace(guidpath, '').count(os.sep)
+		indent = ' ' * 2 * (level)
+		outputfile.write('{}{}'.format(indent, os.path.basename(root) + ':\n'))
+		subindent = ' ' * 2 * (level + 1)
+		for f in files:
+			outputfile.write('{}{}'.format(subindent, f) + '\n')
 
 # get persistence from run key
 hkcurun = "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
 reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
 regkey = winreg.OpenKey(reg, r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run")
 enumkeys = winreg.EnumValue(regkey, 0)
-formatpers = str(enumkeys).replace(',','\n\n') 
 print('\nPossible Persistence found in: ' + hkcurun + '\n')
 
 # save info and tidy up.
-writekeyinfo = outputfile.write('\nPersistence via: \n' + hkcurun + '\n\n ' + str(formatpers))
+outputfile = open(filepath, 'a+')
+writekeyinfo = outputfile.write('\n\nPersistence via: \n' + hkcurun + '\n\nValue name: ' + str(enumkeys[0]) + '\nValue data: '  + str(enumkeys[1]))
 outputfile.close()
 os.remove(stringsfilepath)
 os.startfile(filepath)
