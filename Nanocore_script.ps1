d6eb535e7dd664c1c554a6c7a4ce906e71bf9560
tbxtbxtbx <tbx1066@gmail.com>
2019-09-10 15:25:51 +0100

    added md5 hash to ioc file

--- Nanocore_script.ps1
+++ Nanocore_script.ps1
@@ -5,10 +5,11 @@
 <# Get NanocorePID and do initial checks #>
 $NanoPID = Read-Host -Prompt 'Enter the PID of the Nanocore Process'
 $NanoCheck = 
-$NanoName = Get-Process -Id $NanoPID | Select-Object -ExpandProperty ProcessName
+$NanoName = gps -Id $NanoPID | select -ExpandProperty ProcessName
 Write-Host 'Nanocore process name is:' $NanoName
 $IOCSFile = "$env:USERPROFILE\Desktop\Nanocore_IOCS.txt"
 $NanoStrings = "$env:USERPROFILE\Desktop\Nanocore_Strings.txt"
+$NanoPath = (gps -id $NanoPID).Path
 
 <# Create IOCs file if it doesnt already exist #>
 if (!(Test-Path $IOCSFile))
@@ -22,6 +23,13 @@ else
 }
 ac $IOCSFile 'Nanocore process name is:'; ac $IOCSFile $NanoName;
 
+
+<# Get hash of file #>
+$NanoPath = (gps -id $NanoPID).Path
+$NanoHash = (Get-FileHash -Algorithm MD5 $NanoPath).Hash
+$HashFormat = -join("MD5:","$NanoHash")
+ac $IOCSFile $NanoPath; ac $IOCSFile $HashFormat
+
 <# Dump strings of nanocore process #>
 Write-Host "`nDumping strings from memory"
 strings2 -pid $NanoPID > $NanoStrings
@@ -32,7 +40,7 @@ $findlines = gc $NanoStrings | select-string "PrimaryConnection" -context 0,3
 ac $IOCSFile "`nPotential C2s found:"; ac $IOCSFile $findlines
 
 <# Get Guid #>
-$MachGuid = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Cryptography | Select-Object -ExpandProperty MachineGuid
+$MachGuid = gp -Path HKLM:\SOFTWARE\Microsoft\Cryptography | Select-Object -ExpandProperty MachineGuid
 $NanoFolder = -join ("$env:APPDATA","\","$MachGuid")
 Write-Host "`nChecking for created files/folders"
 ac $IOCSFile "`nFolder structure matching MachineGuid found: "
@@ -44,7 +52,7 @@ ac $IOCSFile -Value "`nPossible persistence found: "
 reg query HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Out-File -Append $IOCSFile 
 
 <# Clean Up files/content #>
-(Get-Content $IOCSFile) -replace "`0", "" | Set-Content $IOCSFile
+(gc $IOCSFile) -replace "`0", "" | sc $IOCSFile
 Write-Host "`nOpening IOC file"
 ri $NanoStrings
 ii $IOCSFile
